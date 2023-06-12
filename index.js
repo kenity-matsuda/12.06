68540ebc97b012b48ac38482289e025ff489ed75
kenity-matsuda <132703873+kenity-matsuda@users.noreply.github.com>
2023-06-12 15:29:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById ("kenity") 
+const x1 = document.getElementById ("container") 
+function mostrarMensagem (){
+    x1.innerHTML = " <h1>palmeiras não tem mundial<h1>"
+}
